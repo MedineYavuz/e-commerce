858083482a89682b4f59672eb6ae8102de73a454
medine yavuz <medineyldr.21@gmail.com>
2023-11-26 17:27:36 +0300

    "js dosyası eklendi"

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//! home sidebar start
+const btnOpenSidebar=document.querySelector("#btn-menu");
+const sidebar=document.querySelector("#sidebar");
+const btnCloseSidebar=document.querySelector("#close-sidebar");
+
+btnOpenSidebar.addEventListener("click",function(){
+   sidebar.style.left = "0";
+   
+});
+
+btnCloseSidebar.addEventListener("click",function(){
+    sidebar.style.left = "-100%";
+})
+
+// click outside star
+
+document.addEventListener("click",function(event){
+    if(!event.composedPath().includes(sidebar)&& !event.composedPath().includes(btnOpenSidebar)){
+        sidebar.style.left = "-100%";
+    }
+})
+
+// click outside end
+
+
+
+
+
+//! home sidebar end
