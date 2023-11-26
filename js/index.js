47971ee39bee7d920608066784230e282888fd61
medine yavuz <medineyldr.21@gmail.com>
2023-11-26 20:32:45 +0300

    "js-yeni-bilgi"

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ btnCloseSidebar.addEventListener("click",function(){
     sidebar.style.left = "-100%";
 })
 
-// click outside star
+// click outside start
 
 document.addEventListener("click",function(event){
     if(!event.composedPath().includes(sidebar)&& !event.composedPath().includes(btnOpenSidebar)){
@@ -22,8 +22,33 @@ document.addEventListener("click",function(event){
 
 // click outside end
 
-
-
-
-
 //! home sidebar end
+
+//! search modal star
+const btnOpenSearch = document.querySelector(".search-toggle");
+const modalSearch = document.getElementsByClassName("modal-search");
+const btnCloseSearch=document.querySelector("#close-search");
+const modalSearchWrapper = document.getElementsByClassName("modal-search-wrapper");
+
+btnOpenSearch.addEventListener("click",function(){
+   modalSearch[0].style.visibility = "visible"
+   modalSearch[0].style.opacity = "1"
+});
+
+btnCloseSearch.addEventListener("click",function(){
+    modalSearch[0].style.visibility = "hidden"
+    modalSearch[0].style.opacity = "0"
+
+ });
+
+// click outside start
+document.addEventListener("click",function(e){
+    if(!e.composedPath().includes(modalSearchWrapper[0]) && !e.composedPath().includes(btnOpenSearch)){
+        modalSearch[0].style.visibility = "hidden"
+        modalSearch[0].style.opacity = "0"
+    }
+})
+
+
+//! search modal end
+
